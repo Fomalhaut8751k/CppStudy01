d4c66a139c3c3dbb768d4f2d561cb1539256c664
WangZhongDe <1308041515@qq.com>
2025-02-01 10:59:49 +0800

    third commit

--- 18 模拟c++的string类代码/main.cpp
+++ 18 模拟c++的string类代码/main.cpp
@@ -1,3 +1,4 @@
+#pragma warning( disable : 4996)
 #include<iostream>
 
 using namespace std;
@@ -6,14 +7,21 @@ using namespace std;
 class String {
 public:
 	String(const char* p = nullptr) {
-
+		if (p == nullptr) {
+			_pstr = new char[1];
+			*_pstr = '\0';
+		}
+		else{
+			_pstr = new char[strlen(p) + 1];  // 有效字符个数+末尾'\0'
+			strcpy(_pstr, p);
+		}
 	}
 private:
 	char* _pstr; 
 };
 
 
-int main() {
+int main(){
 
 
 
